6bdea37d2f3ebeb242774e46d6d926d85c57ac54
Souhail Merroun <souhail.merroun1996@gmail.com>
2021-12-19 17:56:00 +0100

    Search by tag or term small example

--- bin/index.js
+++ bin/index.js
@@ -1,3 +1,44 @@
 #!/usr/bin/env node
 
-console.log("Hello from command-line-shortcuts!");
+/**
+ * requirements
+ */
+const yargs = require("yargs");
+
+/**
+ * params
+ */
+const options = yargs
+    .usage("Usage: -s <term>")
+    .option("s", { alias: "search", describe: "Search by term", type: "string" })
+    .option("t", { alias: "tag", describe: "Search by tag", type: "string" })
+    .argv;
+
+/**
+ * handle search
+ */
+
+const commands = [
+    { shortcut: "delete folder ubuntu", command: "sudo rm -rf storage", tag: ["ubuntu"] },
+    { shortcut: "nvm exec", command: "nvm exec 14.18.1 npm --version", tag: ["node"] },
+    { shortcut: "git pull rebase", command: "git pull --rebase origin master", tag: ["git"] },
+    { shortcut: "", command: "git push origin feat/WR-1454-newsletter-page -f", tag: ["git"] },
+    { shortcut: "", command: "git log --oneline", tag: ["git"] },
+    { shortcut: "", command: "git rebase -i HEAD~25", tag: ["git"] },
+    { shortcut: "", command: "nvm use 14.18.1", tag: ["node", "nvm"] },
+];
+
+if (options.search) {
+    console.log(`You've searched by term for ${options.search}`)
+    console.log('Search by term is not implemented yet')
+} else if (options.tag) {
+    console.log(`You've searched by tag for ${options.tag}`)
+    console.log(`Here are the results:`)
+    commands.forEach(element => {
+        if(element.tag.includes(options.tag)) {
+            console.log(element.command)
+        }
+    });
+} else {
+    console.log('You can either search by term or tag');
+}
